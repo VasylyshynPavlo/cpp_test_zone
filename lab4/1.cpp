ad536eae14be9bfda3955a387e9385e9569f4b4b
VasylyshynPavlo <pavlikvstaras@gmail.com>
2024-10-16 18:31:09 +0300

    add new

--- lab4/1.cpp
+++ lab4/1.cpp
@@ -1,15 +1,18 @@
 #include <ncurses.h>
 
+#include <clocale>
 #include <cmath>
 
 int main() {
+	setlocale(LC_ALL, "");
 	initscr();
-	cbreak();
 	echo();
 
 	int X, Y;
 
-	printw("a) 2.3 <= t <= 7.2\n");
+	printw("a)\n");
+	printw("2.3 ≤ t ≤ 7.2\n");
+	printw("∆t = 0.8\n");
 
 	getyx(stdscr, Y, X);
 
@@ -17,20 +20,40 @@ int main() {
 
 	int i = 0;
 	int step = 13;
-	float t = 2.3, y = 0.0;
+	int header_step = 4;
+	float t = 2.3, y = 0.0, delta_t = 0.8;
 	while (t <= 7.2) {
 		y = pow(cos(pow(t, 2)), 3) / (1.5 * t + 2);
-		move(Y, step * i + 5);
+		move(Y, step * i + header_step);
 		printw("t");
 		move(Y + 1, step * i);
 		printw("%f", t);
-		move(Y + 2, step * i + 5);
-		printw("x");
+		move(Y + 2, step * i + header_step);
+		printw("y");
 		move(Y + 3, step * i);
 		printw("%f", y);
 		i++;
 		refresh();
-		t += 0.8;
+		t += delta_t;
+	}
+
+	t = 0;
+	y = 0;
+	int n = 5;
+	delta_t = 0.3;
+	printw("\n\nb)\nt ≥ %f\nΔt = %f\nn = %x\n", t, delta_t, n);
+	getyx(stdscr, Y, X);
+	for (int i = 0; i < n; i++) {
+		y = pow(cos(pow(t, 2)), 3) / (1.5 * t + 2);
+		move(Y, step * i + header_step);
+		printw("t");
+		move(Y + 1, step * i);
+		printw("%f", t);
+		move(Y + 2, step * i + header_step);
+		printw("y");
+		move(Y + 3, step * i);
+		printw("%f", y);
+		t += delta_t;
 	}
 
 	printw("\nPress any key to close...\n");
